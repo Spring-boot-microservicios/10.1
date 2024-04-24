10d30e264866e0a094a4ab5e06651aa4ade1fc32
Luis Angel <angelfgdeveloper@gmail.com>
2024-04-23 23:52:31 -0600

    Configuracion inicial para hacer test

---
+++ src/test/java/com/debuggeandoideas/airdnd/Test/TestApp.java
@@ -0,0 +1,12 @@
+package com.debuggeandoideas.airdnd.Test;
+
+import org.junit.jupiter.api.Test;
+
+public class TestApp {
+
+    @Test
+    public void Test() {
+        System.out.println("Hola Mundo");
+    }
+
+}
