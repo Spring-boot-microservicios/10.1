945756e41d58f20e039e83143347efa6eb3dec3d
Luis Angel <angelfgdeveloper@gmail.com>
2024-04-24 01:25:14 -0600

    Generando varios mocks e inyecciones

--- src/test/java/com/debuggeandoideas/airdnd/Services/RoomServiceTest.java
+++ src/test/java/com/debuggeandoideas/airdnd/Services/RoomServiceTest.java
@@ -5,21 +5,39 @@ import com.debuggeandoideas.airdnd.utils.DataDummy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+/**
+ * Si queremos generar un mock que inyecta varias dependencias, integrar
+ * @ExtendWith(MockitoExtension.class)
+ *
+ * @Mock Añadir el mock
+ * private RoomRepository roomRepositoryMock;
+ *
+ * @InjectMocks Integrarle la inyeccion
+ * private RoomService roomService;
+ */
+@ExtendWith(MockitoExtension.class)
 public class RoomServiceTest {
 
+    @Mock
     private RoomRepository roomRepositoryMock;
+
+    @InjectMocks
     private RoomService roomService; // objetivo a testear
 
     // BeforeAll se ejecuta antes de todos los test
-    @BeforeEach
-    public void init() {
-        this.roomRepositoryMock = mock(RoomRepository.class); // Realizar mock
-        this.roomService = new RoomService(roomRepositoryMock);
-    }
+//    @BeforeEach
+//    public void init() {
+//        this.roomRepositoryMock = mock(RoomRepository.class); // Realizar mock
+//        this.roomService = new RoomService(roomRepositoryMock);
+//    }
 
     @Test
     @DisplayName("Should get all rooms available in repository")
